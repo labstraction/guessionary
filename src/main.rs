26ec3a639394d71014857904d99ceae015b55f46
labstraction <labstraction@protonmail.com>
2021-08-20 19:50:43 +0200

    new word file

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,39 @@
-use std::fs::File;
-use std::io::Write;
+// use std::fs::File;
+// use std::io::Write;
 
 static HOST_FILE: &'static str = include_str!("../assets/ita.txt");
 
+
 fn main() {
 
-    let lines: Vec<&str> = HOST_FILE.lines().map(|line| {
-        line.split("/")
-        .enumerate()
-        .filter(|&(i, _ )| i == 0)
-        .map(|( _ ,line)| line)
-        .collect::<Vec<&str>>()[0]
+    let lines: Vec<(usize, &str)> = HOST_FILE.split(",").enumerate().collect();
 
-    }).collect();
-
-
-    let path = "results.txt";
-    let mut output = File::create(path).unwrap();
 
     for line in lines {
-        write!(output, "{},", line).unwrap();  
+        print!("{} - {} \n", line.0, line.1)
     }
 
     
 }
+
+// fn main() {
+
+//     let lines: Vec<&str> = HOST_FILE.lines().map(|line| {
+//         line.split("/")
+//         .enumerate()
+//         .filter(|&(i, _ )| i == 0)
+//         .map(|( _ ,line)| line)
+//         .collect::<Vec<&str>>()[0]
+
+//     }).collect();
+
+
+//     let path = "results.txt";
+//     let mut output = File::create(path).unwrap();
+
+//     for line in lines {
+//         write!(output, "{},", line).unwrap();  
+//     }
+
+    
+// }
