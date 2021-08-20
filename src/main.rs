80ac19b0bd738747f63b21610e57bcde24acd928
labstraction <labstraction@protonmail.com>
2021-08-20 19:43:55 +0200

    file parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::fs::File;
+use std::io::Write;
+
+static HOST_FILE: &'static str = include_str!("../assets/ita.txt");
+
 fn main() {
-    println!("Hello, world!");
+
+    let lines: Vec<&str> = HOST_FILE.lines().map(|line| {
+        line.split("/")
+        .enumerate()
+        .filter(|&(i, _ )| i == 0)
+        .map(|( _ ,line)| line)
+        .collect::<Vec<&str>>()[0]
+
+    }).collect();
+
+
+    let path = "results.txt";
+    let mut output = File::create(path).unwrap();
+
+    for line in lines {
+        write!(output, "{},", line).unwrap();  
+    }
+
+    
 }
